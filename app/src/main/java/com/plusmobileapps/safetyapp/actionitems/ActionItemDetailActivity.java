45d72f8820d4d7db830ab87a3e434468396c20ad
Robert Beerman <rbeerma@asu.edu>
2017-12-17 20:39:56 -0700

    - Add ActionItemDetailActivity and associated layout, called as   Intent from ActionItemAdapter when an Action Item card is   clicked. - Add app bar, title, rating, timestamp, priority, and Edit   Priority button to Action Item detail activity.

---
+++ app/src/main/java/com/plusmobileapps/safetyapp/actionitems/ActionItemDetailActivity.java
@@ -0,0 +1,23 @@
+package com.plusmobileapps.safetyapp.actionitems;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+
+import com.plusmobileapps.safetyapp.R;
+
+public class ActionItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent callingIntent = getIntent();
+        setContentView(R.layout.activity_action_item_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.action_item_detail_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.title_action_item_detail);
+
+    }
+}
