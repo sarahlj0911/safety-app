4a83afeaa23a3bcdaf412d03c43ee16c1e69fe17
Robert Beerman <rbeerma@asu.edu>
2017-12-29 10:22:25 -0700

    1. Make Edit Priority button functional, so that the status indicator    in the action item detail view changes according to the selected    priority. 2. Remove layout file for the edit priority dialog in favor of a simple    text list, as the layout was needlessly over-complicating things.

--- app/src/main/java/com/plusmobileapps/safetyapp/actionitems/ActionItemDetailActivity.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/actionitems/ActionItemDetailActivity.java
@@ -1,28 +1,30 @@
 package com.plusmobileapps.safetyapp.actionitems;
 
 import android.support.v4.app.DialogFragment;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.plusmobileapps.safetyapp.R;
 
-public class ActionItemDetailActivity extends AppCompatActivity {
+public class ActionItemDetailActivity extends AppCompatActivity
+        implements EditPriorityDialogFragment.PriorityDialogListener{
     Button editPriorityBtn;
+    View statusDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent callingIntent = getIntent();
         setContentView(R.layout.activity_action_item_detail);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.action_item_detail_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.title_action_item_detail);
 
+        statusDot = findViewById(R.id.action_item_status);
         editPriorityBtn = findViewById(R.id.edit_priority_btn);
 
     }
@@ -30,5 +32,26 @@ public class ActionItemDetailActivity extends AppCompatActivity {
     public void showEditPriorityDialog(View view) {
         DialogFragment editPriorityDialog = new EditPriorityDialogFragment();
         editPriorityDialog.show(getSupportFragmentManager(), "EditPriorityDialogFragment");
+    }
+
+    @Override
+    public void onItemSelected(DialogFragment dialog, CharSequence selectedItem) {
+        EditPriorityDialogFragment priorityDialog = (EditPriorityDialogFragment) dialog;
+        String selectedPriority = priorityDialog.getSelectedItem().toString();
+        Log.d(this.getClass().toString(), "Item selected: " + priorityDialog.getSelectedItem().toString());
+
+        switch (selectedPriority) {
+            case "High":
+                statusDot.setBackgroundResource(R.drawable.circle_red);
+                break;
+            case "Medium":
+                statusDot.setBackgroundResource(R.drawable.circle_yellow);
+                break;
+            case "Low":
+                statusDot.setBackgroundResource(R.drawable.circle_green);
+                break;
+            default:
+                break;
+        }
     }
 }
