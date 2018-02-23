fddf9df7e7ebbe80c64d9f3b5f634c639011a28c
Aaron <a.musengo2@gmail.com>
2018-02-22 22:12:56 -0700

    removed unused method

--- app/src/main/java/com/plusmobileapps/safetyapp/walkthrough/location/LocationModel.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/walkthrough/location/LocationModel.java
@@ -34,5 +34,5 @@ public class LocationModel extends AsyncTask<Void, Void, List<Location>> {
     protected void onPostExecute(List<Location> locations) {
         this.locations = locations;
     }
-    
+
 }
