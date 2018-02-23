27d1118f5722b8b6bb6d9970af4abca40c8c1b25
Aaron <a.musengo2@gmail.com>
2018-02-22 19:52:47 -0700

    Removed the mock location model and created the location model that will load locations via the dao.

--- app/src/main/java/com/plusmobileapps/safetyapp/walkthrough/location/LocationPresenter.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/walkthrough/location/LocationPresenter.java
@@ -1,14 +1,27 @@
 package com.plusmobileapps.safetyapp.walkthrough.location;
 
+
+import android.os.AsyncTask;
+
+import com.plusmobileapps.safetyapp.data.entity.Location;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class LocationPresenter implements LocationContract.Presenter {
 
     private LocationContract.View view;
-    private ArrayList<Location> locations = new LocationFakeModel().getLocations();
+    private AsyncTask<Void, Void, List<Location>> loadLocations = new LocationModel().execute();
+    private List<Location> locations;
 
     public LocationPresenter(LocationContract.View view) {
         this.view = view;
+        try {
+            this.locations = loadLocations.get();
+        } catch (InterruptedException | ExecutionException e)  {
+            e.printStackTrace();
+        }
         view.setPresenter(this);
     }
 
@@ -24,7 +37,7 @@ public class LocationPresenter implements LocationContract.Presenter {
 
     @Override
     public void locationClicked(Location location) {
-        view.openRequestedLocation(location.getTitle());
+        view.openRequestedLocation(location.getName());
     }
 
     @Override
