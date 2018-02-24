2d6934724905fdfd109c7e9f5ebb23604c97af90
ehanna2 <ehanna2@asu.edu>
2018-02-24 10:03:29 -0800

    added WalkthroughContract and WalkthroughPresenter

---
+++ app/src/main/java/com/plusmobileapps/safetyapp/surveys/walkthrough/WalkthroughContract.java
@@ -0,0 +1,18 @@
+package com.plusmobileapps.safetyapp.surveys.walkthrough;
+
+/**
+ * Created by ehanna2 on 2/24/2018.
+ */
+
+public interface WalkthroughContract {
+
+    interface View {
+        //TODO implement view methods
+
+    }
+
+    interface Presenter {
+        //TODO implement click events
+    }
+
+}
---
+++ app/src/main/java/com/plusmobileapps/safetyapp/surveys/walkthrough/WalkthroughPresenter.java
@@ -0,0 +1,9 @@
+package com.plusmobileapps.safetyapp.surveys.walkthrough;
+
+/**
+ * Created by ehanna2 on 2/24/2018.
+ */
+
+public class WalkthroughPresenter {
+
+}
