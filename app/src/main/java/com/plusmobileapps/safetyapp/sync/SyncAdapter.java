a253b81b74a364b504e046a59c6e5137ffd92a67
Robert Beerman <rbeerma@asu.edu>
2018-02-25 17:07:01 -0700

    - Added method to test writing to remote db - Successfully connecting to and reading/writing from/to the AWS db - Next up: Writing the sync logic

--- app/src/main/java/com/plusmobileapps/safetyapp/sync/SyncAdapter.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/sync/SyncAdapter.java
@@ -9,12 +9,20 @@ import android.content.SyncResult;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.plusmobileapps.safetyapp.MyApplication;
+import com.plusmobileapps.safetyapp.data.AppDatabase;
+import com.plusmobileapps.safetyapp.data.dao.SchoolDao;
+import com.plusmobileapps.safetyapp.data.entity.School;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Robert Beerman on 2/25/2018.
@@ -25,10 +33,12 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     // Global variables
     // Define a variable to contain a content resolver instance
     ContentResolver mContentResolver;
-
     Connection conn;
+    private AppDatabase db;
 
-    private static final String url = "jdbc:mysql://10.0.2.2:3306/safetywalkthrough";
+    //private static final String url = "jdbc:mysql://10.0.2.2:3306/safetywalkthrough";
+    private static final String url = "jdbc:mysql://safetymysqlinstance.cbcumohyescr.us-west-2.rds.amazonaws.com:3306/safetywalkthrough";
+    private static final String dbName = "safetywalkthrough";
     private static final String user = "safety_app";
     private static final String pass = "";
     /**
@@ -67,13 +77,21 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
             String authority,
             ContentProviderClient provider,
             SyncResult syncResult) {
-
-        testDB();
-    }
-
-    public void testDB() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        db = AppDatabase.getAppDatabase(MyApplication.getAppContext());
+
+        testWriteToDB();
+        testReadFromDB();
+    }
+
+    public void testReadFromDB() {
+        try {
+            //Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection(url, user, pass);
             Log.d(TAG, "Database connection success!");
 
@@ -90,7 +108,51 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
             Log.d(TAG, e.getMessage());
         } finally {
             try {
-                conn.close();
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException sqle) {
+                sqle.printStackTrace();
+                Log.d(TAG, sqle.getMessage());
+            }
+        }
+    }
+
+    public void testWriteToDB() {
+        try {
+            conn = DriverManager.getConnection(url, user, pass);
+            conn.setAutoCommit(true);
+            Log.d(TAG, "Connected to database on " + url);
+
+            SchoolDao schoolDao = db.schoolDao();
+            List<School> schoolList = schoolDao.getAll();
+            int schoolId = 0;
+            String schoolName = "";
+
+            if (schoolList != null && !schoolList.isEmpty()) {
+                School school = schoolList.get(0);
+                schoolId = school.getSchoolId();
+                schoolName = school.getSchoolName();
+            }
+
+            PreparedStatement insertSchool = null;
+            String insertSchoolString = "REPLACE INTO " + dbName + ".schools VALUES (?, ?)";
+            insertSchool = conn.prepareStatement(insertSchoolString);
+            insertSchool.setInt(1, schoolId);
+            insertSchool.setString(2, schoolName);
+
+            if (insertSchool.execute()) {
+                Log.d(TAG, "Insert successful!");
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
             } catch (SQLException sqle) {
                 sqle.printStackTrace();
                 Log.d(TAG, sqle.getMessage());
