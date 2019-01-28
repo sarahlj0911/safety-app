8b9c331967b17807301eacaec61b870c6daef7aa
Nathan <nnelson9@asu.edu>
2019-01-27 22:02:12 -0700

    Update AdminSettings.java
    Created options method, will be used for photos

--- app/src/main/java/com/plusmobileapps/safetyapp/AdminSettings.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/AdminSettings.java
@@ -1,7 +1,11 @@
 package com.plusmobileapps.safetyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,6 +37,26 @@ public class AdminSettings extends AppCompatActivity {
             }
 
         });
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu){
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.drop_down_menu,menu);
+            return super.onCreateOptionsMenu(menu);
+        }
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item){
+
+            switch(item.getItemId()){
+                case R.id.settings_menu:
+                    //settings selected
+                    Intent adminsettings = new Intent(this, AdminSettings.class);
+                    startActivity(adminsettings);
+                    break;
+            }
+
+
+            return super.onOptionsItemSelected(item);
+        }
 
 
 
