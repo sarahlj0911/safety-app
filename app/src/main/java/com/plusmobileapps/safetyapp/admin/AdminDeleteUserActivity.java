c92954a8a5759d4af94d30ff9bdc6afedf2b033b
ShareefLevon <shareef.levon@asu.edu>
2019-02-15 20:54:04 -0700

    task-106
    mockup of spinner that will list users to delete.

--- app/src/main/java/com/plusmobileapps/safetyapp/admin/AdminDeleteUserActivity.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/admin/AdminDeleteUserActivity.java
@@ -3,21 +3,39 @@ package com.plusmobileapps.safetyapp.admin;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.plusmobileapps.safetyapp.PrefManager;
 import com.plusmobileapps.safetyapp.R;
 
 public class AdminDeleteUserActivity extends AppCompatActivity {
-
+    String array[] = {"user1", "user2", "user3", "user4"};
     Button b1;
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_admin_delete_user);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.userArray,android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
 
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), (CharSequence) parent.getItemAtPosition(position),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
-
 }
