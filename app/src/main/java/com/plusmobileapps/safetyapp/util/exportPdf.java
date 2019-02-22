12c928becf870c081ee25aec3343261b2e80315f
Nathan <nnelson9@asu.edu>
2019-02-21 21:36:07 -0700

    Update exportPdf.java
    Export testing with integrated Android PDF library

--- app/src/main/java/com/plusmobileapps/safetyapp/util/exportPdf.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/util/exportPdf.java
@@ -1,5 +1,7 @@
 package com.plusmobileapps.safetyapp.util;
 
+import android.graphics.Rect;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -24,10 +26,20 @@ public class exportPdf extends AppCompatActivity {
     protected CheckBox WalkthroughCommentsbox;
     protected CheckBox Picsbox;
     public DataExtractor extractor = new DataExtractor();
+    String Results = "";
+
     //public Document doc = new Document();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        PdfDocument document = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(new Rect(0, 0, 100, 100), 1).create();
+
+        PdfDocument.Page page = document.startPage(pageInfo);
+        View content = getContentView();
+        content.draw(page.getCanvas());
+        document.writeTo(getOutputStream());
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export_pdf);
         ActionItembox = findViewById(R.id.checkBoxActionItems);
@@ -38,8 +50,8 @@ public class exportPdf extends AppCompatActivity {
 
 
     public void export(){
-        extractor.getlist();
-
+        Results = extractor.getlist();
+        
 
     }
 }
