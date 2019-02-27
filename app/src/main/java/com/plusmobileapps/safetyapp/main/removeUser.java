b0b54bce493fb662e2e9bb8dde561df06607bfd6
bskoczyl <bskoczylas0915@gmail.com>
2019-02-26 20:48:14 -0700

    remove user backend

--- app/src/main/java/com/plusmobileapps/safetyapp/main/removeUser.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/main/removeUser.java
@@ -1,20 +1,29 @@
 package com.plusmobileapps.safetyapp.main;
 
 
-import android.content.Intent;
+import android.content.Context;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.GetDetailsHandler;
+import com.plusmobileapps.safetyapp.AwsServices;
 import com.plusmobileapps.safetyapp.R;
 
 public class removeUser extends AppCompatActivity {
+
+    private CognitoUserPool userPool;
+    private AwsServices awsServices;
+    private Context CONTEXT = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +44,51 @@ public class removeUser extends AppCompatActivity {
         ab.setDisplayHomeAsUpEnabled(true);
 
 
-
-
-
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
+                awsServices = new AwsServices();
+                initAWSUserPool();
+
+                CognitoUser user = userPool.getUser("bskyl@asu.edu");
+
+
+
+                GetDetailsHandler handler = new GetDetailsHandler() {
+                    @Override
+                    public void onSuccess(final CognitoUserDetails list) {
+                        Log.d("finduser", "Successfully retrieved user details");
+                    }
+
+                    @Override
+                    public void onFailure(final Exception exception) {
+                        Log.d("finduser", "Failed to retrieve the user details, probe exception for the cause");
+                    }
+                };
+                user.getDetails(handler);
+
+                /*GenericHandler handler = new GenericHandler() {
+
+                    @Override
+                    public void onSuccess() {
+                        // Delete was successful!
+                    }
+
+                    @Override
+                    public void onFailure(Exception exception) {
+                        // Delete failed, probe exception for details
+                    }
+                };
+                user.deleteUser(handler);*/
+
 
                 Snackbar mySnackbar = Snackbar.make(view, R.string.user_not_found, Snackbar.LENGTH_SHORT);
                 mySnackbar.show();
             }
         });
+    }
+
+    private void initAWSUserPool(){
+        userPool = new CognitoUserPool(CONTEXT, awsServices.getPOOL_ID(), awsServices.getAPP_ClIENT_ID(), awsServices.getAPP_ClIENT_SECRET(), awsServices.getREGION());
     }
 
 }
