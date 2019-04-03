490db2d146f5c84b6e5cf764e0689754106a9e2e
Shareef Levon <shareeflevon@yahoo.com>
2019-04-02 23:49:07 -0700

    Task-214
    Delete user updates to UI and design

--- app/src/main/java/com/plusmobileapps/safetyapp/admin/AdminDeleteUserActivity.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/admin/AdminDeleteUserActivity.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
@@ -18,7 +19,7 @@ import com.plusmobileapps.safetyapp.PrefManager;
 import com.plusmobileapps.safetyapp.R;
 
 public class AdminDeleteUserActivity extends AppCompatActivity {
-    String array[] = {"user1", "user2", "user3", "user4","user5"};
+  TextView email;
     Button b1;
     CognitoUserPool userPool;
     Spinner spinner;
@@ -29,28 +30,20 @@ public class AdminDeleteUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_delete_user);
         userPool = new AwsServices().initAWSUserPool(this);
         //user id is the email
-        userPool.getUser("asasasas").deleteUser(deleteUserCallback);
+
         //creates spinner to list users
-        spinner = (Spinner) findViewById(R.id.spinner);
-        adapter = ArrayAdapter.createFromResource(this, R.array.userArray,android.R.layout.simple_spinner_item);
-        spinner.setAdapter(adapter);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getBaseContext(), (CharSequence) parent.getItemAtPosition(position),Toast.LENGTH_LONG).show();
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
-            }
-        });
     }
     public void backClicked(){
         Intent i=new Intent(AdminDeleteUserActivity.this,AdminMainActivity.class);
         startActivity(i);
 
+    }
+    public void onClickDelete(){
+        email=(findViewById(R.id.editText3));
+        userPool.getUser(email.getText().toString()).deleteUser(deleteUserCallback);
     }
     GenericHandler deleteUserCallback = new GenericHandler() {
         @Override
