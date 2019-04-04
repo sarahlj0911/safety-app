3dee36005eb1e9d756a5b079e113b77f5a40015e
Nathan <nnelson9@asu.edu>
2019-04-04 09:42:36 -0700

    Revert "Bug fix for floating action button"
    This reverts commit 0515d8f5ef5677b30e73358009cc61ae2a895ff4.

--- app/src/main/java/com/plusmobileapps/safetyapp/walkthrough/landing/WalkthroughLandingFragment.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/walkthrough/landing/WalkthroughLandingFragment.java
@@ -95,10 +95,9 @@ public class WalkthroughLandingFragment extends Fragment
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         adapter = new WalkthroughLandingAdapter(new ArrayList<Walkthrough>(0), itemListener);
         recyclerView.setAdapter(adapter);
-
         fab.setOnClickListener(fabListener);
         progressBar = rootView.findViewById(R.id.pb_loading_indicator);
-        fab.bringToFront();
+
         return rootView;
     }
 
