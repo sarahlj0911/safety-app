ae815a3d1cdcd1659f37816960ccc456b552410a
Jeremy Powell <jcpowel5@asu.edu>
2019-04-25 16:10:30 -0700

    Animation Changes: Faster activity fade transitions Fixed 50% black screen when fading

--- app/src/main/java/com/plusmobileapps/safetyapp/main/MainActivity.java
+++ app/src/main/java/com/plusmobileapps/safetyapp/main/MainActivity.java
@@ -266,7 +266,7 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
     private void launchLoginScreen() {
         Intent loginActivity = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginActivity);
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out_none);
         finish();
     }
 
